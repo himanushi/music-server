e35dfdb7538ce720757ca2668d01e116adc4649f
himanushi <himanushi@users.noreply.github.com>
2020-03-19 00:09:00 +0900

    TTID 大幅仕様変更

--- app/models/concerns/ttid.rb
+++ app/models/concerns/ttid.rb
@@ -1,18 +1,16 @@
 # TTID
-# Timestamp and Table Hex ID
+# Table id and hex Timestamp and hex ID
 module TTID
   extend ActiveSupport::Concern
 
-  REGEXP = /\A([a-z0-9]{10})([A-Za-z0-9_-]{5})([A-Za-z0-9_-]{5})\z/
-
-  class DuplicateDefinedTableIdError < StandardError; end
+  REGEXP = /\A([a-z]{4})([0-9a-f]{14})([0-9a-f]{6})\z/
 
   class << self
     def to_hash(id)
       return nil unless REGEXP =~ id
       {
-        hex_timestamp: Regexp.last_match[1],
-        table_id:      Regexp.last_match[2],
+        table_id:      Regexp.last_match[1],
+        hex_timestamp: Regexp.last_match[2],
         hex_id:        Regexp.last_match[3],
       }
     end
@@ -20,14 +18,14 @@ module TTID
     def decode(id)
       return nil unless result = to_hash(id)
       {
-        timestamp: to_timestamp(result[:hex_timestamp]),
         model:     table_info[result[:table_id]],
+        timestamp: to_timestamp(result[:hex_timestamp]),
         hex_id:    result[:hex_id],
       }
     end
 
     def to_timestamp(hex_timestamp)
-      Time.at(hex_timestamp.to_i(36).to_f / 100000)
+      Time.at(hex_timestamp.to_i(16).to_f / 1000000)
     end
 
     def table_info
@@ -35,10 +33,17 @@ module TTID
     end
 
     def add_table_info(info)
-      if table_info.has_key?(info.keys.first)
-        raise DuplicateDefinedTableIdError, "テーブル Hex が重複しました。別のテーブルIDに変更してください。"
+      if table_info.has_key?(key = info.keys.first)
+        raise StandardError, "テーブルIDが重複しました。別のテーブルIDに変更してください。[#{ key }]"
       end
       @_table_info = table_info.merge(info)
+    end
+
+    def unix_timestamp_usec6
+      # IDの採番はDBの時間基準とする
+      # 複数インスタンスを立ち上げた場合にアプリ基準の時間にすると不整合が発生する可能性があるため
+      # TODO: 性能に問題がある場合は DB で関数を作成し ID を生成すること
+      ActiveRecord::Base.connection.select_value("SELECT CAST((UNIX_TIMESTAMP(NOW(6)) * 1000000) AS INTEGER)")
     end
   end
 
@@ -56,30 +61,26 @@ module TTID
 
   class_methods do
     def table_id(_table_id = nil)
-      if @table_id.present? && _table_id.present?
-        raise DuplicateDefinedTableIdError, "テーブルIDは重複して宣言できません"
-      elsif @table_id.nil? && _table_id.nil?
-        raise NotImplementedError, "テーブルIDを必ず実装し絶対に変更しないこと"
-      end
+      return @table_id.dup if @table_id.present?
+
+      raise NotImplementedError, "テーブルIDを必ず実装すること" unless _table_id.present?
+      raise StandardError, "テーブルIDは /[a-z]{4}/ にすること[#{_table_id}]" unless /[a-z]{4}/.match?(_table_id)
+
+      _table_id = _table_id.to_s
 
       @table_id ||= begin
-        id = _table_id.to_s
-        TTID.add_table_info({ "#{crypt_table_id(id)}" => self })
-        id
+        TTID.add_table_info({ _table_id => self })
+        _table_id
       end
-    end
-
-    def crypt_table_id(id = nil)
-      @crypt_table_id ||= Base64.urlsafe_encode64(id || table_id)[...5]
     end
 
     def hex_time
-      # 3128-08-04 11:40:00 まで使用可能
-      (Time.now.to_f * 100000).to_i.to_s(36)
+      # 4253-05-31 22:20:37 まで使用可能
+      TTID.unix_timestamp_usec6.to_s(16).rjust(14, "0")
     end
 
     def build_id
-      hex_time << crypt_table_id << SecureRandom.urlsafe_base64(4, false)[...5]
+      table_id << hex_time << SecureRandom.hex(3)
     end
   end
 end
