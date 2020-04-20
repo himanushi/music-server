c632c50dc24e4ac53b3fda9b5e8afd6f6f9ab70d
himanushi <himanushi@users.noreply.github.com>
2020-04-20 14:40:35 +0900

    Album mix モジュール追加

---
+++ app/models/concerns/albums/mix.rb
@@ -0,0 +1,25 @@
+module Albums
+  module Mix
+    extend ActiveSupport::Concern
+
+    class_methods do
+      def mix(main_album_id, sub_album_id)
+        main_album = find(main_album_id)
+        sub_album  = find(sub_album_id)
+
+        if main_album.apple_music_and_itunes_album.nil?
+          main_album.apple_music_and_itunes_album = sub_album.apple_music_and_itunes_album
+        end
+
+        if main_album.spotify_album.nil?
+          main_album.spotify_album = sub_album.spotify_album
+        end
+
+        ActiveRecord::Base.transaction do
+          main_album.save!
+          sub_album.destroy!
+        end
+      end
+    end
+  end
+end
