20bac42d888f36e4c13ca7105f0d85184ed5d213
himanushi <himanushi@users.noreply.github.com>
2020-04-26 20:35:31 +0900

    seed 変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,15 +18,14 @@ puts "seed 開始".red
 
 %w[
   植松伸夫 すぎやまこういち 伊藤賢治 下村陽子 光田康典 古代祐三
-  崎元仁 西木康智 谷岡久美 成田勤 土屋俊輔 上松範康 岡部啓一 祖堅正慶 Revo
+  崎元仁 西木康智 谷岡久美 成田勤 土屋俊輔 上松範康 岡部啓一 祖堅正慶
   岩田匡治 いとうけいすけ なるけみちこ ピース flasygoodness 佐野信義
   ジェイク・カウフマン Inon\ Zur Austin\ Wintory Gareth\ Coker
   Jeremy\ Soule Kristofer\ Maddigan
   SQUARE\ ENIX\ MUSIC CAPCOM カプコン カプコンサウンドチーム アトラスサウンドチーム
-  ファルコム・サウンド・チーム・JDK SNK\ サウンドチーム GUST クラリスディスク コナミ短形波倶楽部
+  ファルコム・サウンド・チーム・JDK Falcom\ Sound\ Team\ jdk SNK\ サウンドチーム GUST クラリスディスク コナミ短形波倶楽部
   colopl INSIDE\ SYSTEM ジョーダウン namco namco\ sounds ベイシスケイプ
   Sony\ Computer\ Entertainment\ Inc.
-  メギド72 ブレイブファンタジア MOTHER3
 ].each do |name|
   puts "create artist #{name}".green
   start_time = Time.now
