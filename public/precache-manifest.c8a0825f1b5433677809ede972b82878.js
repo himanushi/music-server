474fcf36b85002756aff1c8d23d52ca198f48c54
himanushi <himanushi@users.noreply.github.com>
2020-04-29 17:00:57 +0900

    build

---
+++ public/precache-manifest.c8a0825f1b5433677809ede972b82878.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88f2fc64fab1a22695ab7c0263db1790",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e87aeadf63b300f294b",
+    "url": "/static/js/2.ea016bfc.chunk.js"
+  },
+  {
+    "revision": "775071e7908c30b629946039840806b0",
+    "url": "/static/js/2.ea016bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8189a1eb4e62e5c9d72",
+    "url": "/static/js/main.151e445d.chunk.js"
+  },
+  {
+    "revision": "47faa472f9d669b52135",
+    "url": "/static/js/runtime-main.71ab8cba.js"
+  }
+]);
