7a261d7275ee94e887b4ca489d5a6d29750ab3b5
himanushi <himanushi@users.noreply.github.com>
2020-05-01 15:01:57 +0900

    本番でもActiveRecordのログ出力する

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,4 +96,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  ActiveRecord::Base.logger = Logger.new(STDOUT)
+  ActiveRecord::Base.logger.level = 0
 end
