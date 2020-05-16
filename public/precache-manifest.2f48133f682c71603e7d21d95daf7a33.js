90aa1acd3f43319c25fe8a436eef5753949ddf2b
himanushi <himanushi@users.noreply.github.com>
2020-05-16 11:23:10 +0900

    build

--- public/precache-manifest.2f48133f682c71603e7d21d95daf7a33.js
+++ public/precache-manifest.2f48133f682c71603e7d21d95daf7a33.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d167b4b820db08d17e308901dd332727",
+    "revision": "69de43395135739090ced51c13f60c55",
     "url": "/index.html"
   },
   {
-    "revision": "16f4a3e7cd283646284b",
-    "url": "/static/js/2.2cda31e9.chunk.js"
+    "revision": "c2d4941da37410150fb2",
+    "url": "/static/js/2.73a26406.chunk.js"
   },
   {
     "revision": "b3e3f9cf4d9a7f3f2d3ec600471c2c3a",
-    "url": "/static/js/2.2cda31e9.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.73a26406.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "579a6640e9eebf45a1e8",
-    "url": "/static/js/main.905c6666.chunk.js"
+    "revision": "0a42a97c8145e41f980d",
+    "url": "/static/js/main.5246d34f.chunk.js"
   },
   {
     "revision": "47faa472f9d669b52135",
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e90fe8371e3f7acfe78cd015b9f2f196.js"
+  "/precache-manifest.2f48133f682c71603e7d21d95daf7a33.js"
 );
 
 self.addEventListener('message', (event) => {
