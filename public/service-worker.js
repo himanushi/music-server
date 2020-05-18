b134ae24152fe763226cc39337a68c921f33d0c5
himanushi <himanushi@users.noreply.github.com>
2020-05-19 06:49:38 +0900

    build

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.476148b884be2ef029f87f109de56043.js"
+  "/precache-manifest.a076adbf3b464d46b696d62016a86be3.js"
 );
 
 self.addEventListener('message', (event) => {
