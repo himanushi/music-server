fe7af69970ce4868d93405daaf8feca97d58891b
himanushi <himanushi@users.noreply.github.com>
2020-07-09 07:18:49 +0900

    お気に入り一括登録と解除追加

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,4 +2,19 @@ class Favorite < ApplicationRecord
   table_id :fav
 
   belongs_to :favorable, polymorphic: true
+  belongs_to :user
+
+  class << self
+    def register(favorites, user)
+      ActiveRecord::Base.transaction do
+        favorites.each {|favorite| find_or_create_by(favorable: favorite, user: user) }
+      end
+    end
+
+    def unregister(favorites, user)
+      ActiveRecord::Base.transaction do
+        where(favorable: favorites, user: user).destroy_all
+      end
+    end
+  end
 end
