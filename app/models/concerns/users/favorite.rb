5709d03fb724c5c3ff081d259c5eefe8405242fa
himanushi <himanushi@users.noreply.github.com>
2020-07-12 07:51:19 +0900

    お気に入りモジュール追加

---
+++ app/models/concerns/users/favorite.rb
@@ -0,0 +1,26 @@
+# ユーザーお気に入り関連
+module Users
+  module Favorite
+    extend ActiveSupport::Concern
+
+    class UserFavorite
+      attr_reader :user_id
+
+      def initialize(user_id)
+        @user_id = user_id
+      end
+
+      def album_ids
+        ::Favorite.where(user_id: user_id, favorable_type: Album.name).pluck(:favorable_id)
+      end
+
+      def artist_ids
+        ::Favorite.where(user_id: user_id, favorable_type: Artist.name).pluck(:favorable_id)
+      end
+    end
+
+    def favorite
+      UserFavorite.new(self.id)
+    end
+  end
+end
