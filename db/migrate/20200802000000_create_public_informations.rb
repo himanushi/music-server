8c1e441d7dbbc41b2f1278555b708918d49c6a74
himanushi <himanushi@users.noreply.github.com>
2020-08-02 17:47:31 +0900

    公開情報モデル追加

---
+++ db/migrate/20200802000000_create_public_informations.rb
@@ -0,0 +1,12 @@
+class CreatePublicInformations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :public_informations, id: false do |t|
+      t.string     :id, limit: 16, primary_key: true, null: false
+      t.timestamps
+      t.string     :user_id, limit: 16, null: false
+      t.integer    :public_type, null: false
+    end
+    add_foreign_key :public_informations, :users, dependent: :destroy
+    add_index :public_informations, [:user_id, :public_type], unique: true
+  end
+end
