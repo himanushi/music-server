071a15d872b536da6f1db7b7d9a9ff3d3166655e
himanushi <himanushi@users.noreply.github.com>
2020-08-29 08:00:37 +0900

    サーバー実行時の初期実行を追加

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 
+# サーバーが起動するたびに実行したいことを書く
+echo "start migration!!"
+bundle exec rake db:migrate
+echo "end   migration!!"
+
+echo "start generating sitemap!!"
+bundle exec rails runner SitemapGenerator.generate_all
+echo "end   generating sitemap!!"
+
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /music-server/tmp/pids/server.pid
 
