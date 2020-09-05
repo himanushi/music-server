d4d265087d4aa6dc0ea35c3cece3ae4bd37e06c0
himanushi <himanushi@users.noreply.github.com>
2020-09-06 05:34:15 +0900

    add JwtUtil sig

--- lib/jwt_util.rb
+++ lib/jwt_util.rb
@@ -1,7 +1,7 @@
 class JwtUtil
   class << self
     def encode(object)
-      object = { **object, exp: Session::EXPIRE_DAYS.from_now.to_i }.deep_symbolize_keys
+      object = object.merge({ exp: Session::EXPIRE_DAYS.from_now.to_i }).deep_symbolize_keys
       JWT.encode(object, ENV["SECRET_KEY_BASE"], 'HS512')
     end
 
