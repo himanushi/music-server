45b362e15da6b5d16d75a1fa62db00309568063a
himanushi <himanushi@users.noreply.github.com>
2020-10-01 06:27:55 +0900

    更新修正

--- app/graphql/mutations/update_me.rb
+++ app/graphql/mutations/update_me.rb
@@ -2,46 +2,46 @@ class Mutations::UpdateMe < Mutations::BaseMutation
   description "カレントユーザー情報更新"
 
   argument :name, String, required: false
-  argument :username, String, required: false
+
   argument :new_password,  String, required: false
-  argument :old_password, String, required: true
+  argument :old_password, String, required: false
 
   field :current_user, Types::Objects::CurrentUserType, null: true
   field :error, String, null: true
 
   def mutate(**attrs)
     begin
+      ### ユーザー情報更新
+      _attrs = attrs.slice(:name)
+
+      ### パスワード変更
       old_password = attrs.delete(:old_password)
+      new_password = attrs.delete(:new_password)
 
-      # なりすまし対策
-      if attrs[:new_password].present? && context[:current_info][:user].encrypted_password.present?
+      # 現在のパスワードを検証
+      if old_password || new_password
+        raise StandardError, "エラー : 現在のパスワードを入力してください" unless old_password.present?
+        raise StandardError, "エラー : 新しいパスワードを入力してください" unless new_password.present?
+
         hash_password = BCrypt::Password.new(context[:current_info][:user].encrypted_password)
-        raise StandardError unless hash_password == old_password
-      end
+        raise StandardError, "エラー : 現在のパスワードが違います"        unless hash_password == old_password
 
-      # パスワード更新
-      if (new_password = attrs.delete(:new_password)).present?
         attrs[:encrypted_password] = BCrypt::Password.create(new_password, cost: 12)
         # パスワードを変更したら全てのセッションを削除し、最新セッション作成
         context[:current_info][:user].sessions.delete_all
         context[:current_info][:session] = context[:current_info][:user].sessions.create!
       end
 
-      context[:current_info][:user].update!(attrs)
+      context[:current_info][:user].update!(_attrs)
 
       {
         current_user: context[:current_info][:user],
         error: nil,
       }
-    rescue ActiveRecord::RecordInvalid => error
-      {
-        current_user: nil,
-        error: "指定したユーザー名はすでに存在します",
-      }
     rescue => error
       {
         current_user: nil,
-        error: "古いパスワードが違います",
+        error: error.message
       }
     end
   end
