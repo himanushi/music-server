8cce9d452af34703249fec2472822380cabf6c95
himanushi <himanushi@users.noreply.github.com>
2020-10-05 16:11:52 +0900

    ユーザーのパスワードカラム変更

---
+++ db/migrate/20201005000000_change_column_to_users.rb
@@ -0,0 +1,6 @@
+class ChangeColumnToUsers < ActiveRecord::Migration[6.0]
+  def change
+    rename_column :users, :encrypted_password, :password_digest
+    add_column :users, :registered, :boolean, null: false, index: true, default: false
+  end
+end
