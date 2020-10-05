b547ff60f70ec293d24fb5bf8d60470fc0d1eee5
himanushi <himanushi@users.noreply.github.com>
2020-10-05 16:16:48 +0900

    認証方法変更したので色々修正

--- app/graphql/mutations/signup.rb
+++ app/graphql/mutations/signup.rb
@@ -3,8 +3,8 @@ class Mutations::Signup < Mutations::BaseMutation
 
   argument :name, String, required: true
   argument :username, String, required: true
-  argument :new_password,  String, required: true
-  argument :old_password, String, required: true
+  argument :password, String, required: true
+  argument :password_confirmation, String, required: true
 
   field :current_user, Types::Objects::CurrentUserType, null: true
   field :error, String, null: true
@@ -14,18 +14,16 @@ class Mutations::Signup < Mutations::BaseMutation
   def mutate(**attrs)
     begin
       # 登録済みは正常終了しておく
-      raise RegisteredError if context[:current_info][:user].registered?
+      raise RegisteredError if context[:current_info][:user].registered
 
-      # パスワード検証
-      old_password = attrs.delete(:old_password)
-      new_password = attrs.delete(:new_password)
-      raise StandardError, "エラー : パスワードが一致しません" unless old_password == new_password
-
-      # パスワードを設定したら全てのセッションを削除し、最新セッション作成
-      attrs[:encrypted_password] = BCrypt::Password.create(new_password, cost: 12)
-      context[:current_info][:user].sessions.delete_all
-      context[:current_info][:session] = context[:current_info][:user].sessions.create!
-      context[:current_info][:user].update!(attrs)
+      # 設定したら全てのセッションを削除し、最新セッション作成
+      ActiveRecord::Base.transaction do
+        attrs[:registered] = true
+        attrs[:role] = Role.login_role
+        context[:current_info][:user].update!(attrs)
+        context[:current_info][:user].sessions.delete_all
+        context[:current_info][:session] = context[:current_info][:user].sessions.create!
+      end
 
       {
         current_user: context[:current_info][:user],
--- app/graphql/mutations/update_me.rb
+++ app/graphql/mutations/update_me.rb
@@ -3,7 +3,8 @@ class Mutations::UpdateMe < Mutations::BaseMutation
 
   argument :name, String, required: false
 
-  argument :new_password,  String, required: false
+  argument :new_password, String, required: false
+  argument :password_confirmation, String, required: false
   argument :old_password, String, required: false
 
   field :current_user, Types::Objects::CurrentUserType, null: true
@@ -18,21 +19,27 @@ class Mutations::UpdateMe < Mutations::BaseMutation
       old_password = attrs.delete(:old_password)
       new_password = attrs.delete(:new_password)
 
-      # 現在のパスワードを検証
-      if old_password || new_password
-        raise StandardError, "エラー : 現在のパスワードを入力してください" unless old_password.present?
-        raise StandardError, "エラー : 新しいパスワードを入力してください" unless new_password.present?
+      ActiveRecord::Base.transaction do
 
-        hash_password = BCrypt::Password.new(context[:current_info][:user].encrypted_password)
-        raise StandardError, "エラー : 現在のパスワードが違います"        unless hash_password == old_password
+        # 現在のパスワードを検証
+        if old_password.present? || new_password.present?
+          raise StandardError, "エラー : 現在のパスワードを入力してください" unless old_password.present?
+          raise StandardError, "エラー : 新しいパスワードを入力してください" unless new_password.present?
+          raise StandardError, "エラー : 現在のパスワードが違います" unless context[:current_info][:user].authenticate(old_password)
 
-        attrs[:encrypted_password] = BCrypt::Password.create(new_password, cost: 12)
-        # パスワードを変更したら全てのセッションを削除し、最新セッション作成
-        context[:current_info][:user].sessions.delete_all
-        context[:current_info][:session] = context[:current_info][:user].sessions.create!
+          # パスワードを変更したら全てのセッションを削除し、最新セッション作成
+          _attrs[:password] = new_password
+          _attrs[:password_confirmation] = attrs.delete(:password_confirmation) || ""
+        end
+
+        context[:current_info][:user].update!(_attrs)
+
+        # パスワードを変更していた場合
+        if old_password.present? || new_password.present?
+          context[:current_info][:user].sessions.delete_all
+          context[:current_info][:session] = context[:current_info][:user].sessions.create!
+        end
       end
-
-      context[:current_info][:user].update!(_attrs)
 
       {
         current_user: context[:current_info][:user],
