8a01100dfe09cbbbacc2df38d0de11b5245edd74
himanushi <himanushi@users.noreply.github.com>
2020-10-12 12:27:40 +0900

    GraphQL の挙動は本番と合わせておく

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -8,15 +8,10 @@ class GraphqlController < ApplicationController
     }
     result = ServerSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
-  rescue => e
-    raise e unless Rails.env.development?
-    handle_error_in_development e
   end
 
-  private
-
   # Handle form data, JSON body, or a blank value
-  def ensure_hash(ambiguous_param)
+  private def ensure_hash(ambiguous_param)
     case ambiguous_param
     when String
       if ambiguous_param.present?
@@ -31,12 +26,5 @@ class GraphqlController < ApplicationController
     else
       raise ArgumentError, "Unexpected parameter: #{ambiguous_param}"
     end
-  end
-
-  def handle_error_in_development(e)
-    logger.error e.message
-    logger.error e.backtrace.join("\n")
-
-    render json: { error: { message: e.message, backtrace: e.backtrace }, data: {} }, status: 500
   end
 end
