2db74af8e8612a0bb9a5df979c594cbca3779eff
himanushi <himanushi@users.noreply.github.com>
2021-01-23 18:42:50 +0900

    カウント系クエリ追加

---
+++ app/graphql/queries/albums_count.rb
@@ -0,0 +1,27 @@
+module Queries
+  class AlbumsCount < BaseCountQuery
+    description "アルバム件数取得"
+
+    type Integer, null: false
+
+    class AlbumsCountConditionsInputObject < BaseInputObject
+      argument :usernames, [String], "ユーザー名", required: false
+      argument :artists,   IdInputObject, "アーティストID", required: false
+      argument :tracks,    IdInputObject, "トラックID", required: false
+      argument :name,      String, "アルバム名(あいまい検索)", required: false
+      argument :status,    [StatusEnum], "表示ステータス", required: false
+      argument :favorite,  Boolean, "お気に入り", required: false
+    end
+
+    argument :conditions, AlbumsCountConditionsInputObject, required: false, description: "取得条件"
+
+    def count_query(conditions: {})
+      album_relation, conditions, is_cache = Queries::Albums.build_relation(conditions: conditions)
+
+      [
+        is_cache,
+        album_relation.where(conditions)
+      ]
+    end
+  end
+end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,15 +2,16 @@ module Types
   class QueryType < Types::Objects::BaseObject
     field :apple_music_token, resolver: Queries::AppleMusicToken
     field :spotify_token, resolver: Queries::SpotifyToken
-    field :me,        resolver: Queries::Me
-    field :artist,    resolver: Queries::Artist
-    field :artists,   resolver: Queries::Artists
-    field :album,     resolver: Queries::Album
-    field :albums,    resolver: Queries::Albums
-    field :tracks,    resolver: Queries::Tracks
-    field :playlist,  resolver: Queries::Playlist
-    field :playlists, resolver: Queries::Playlists
-    field :my_playlists, resolver: Queries::MyPlaylists
-    field :roles,     resolver: Queries::Roles
+    field :me,            resolver: Queries::Me
+    field :artist,        resolver: Queries::Artist
+    field :artists,       resolver: Queries::Artists
+    field :album,         resolver: Queries::Album
+    field :albums,        resolver: Queries::Albums
+    field :albums_count,  resolver: Queries::AlbumsCount
+    field :tracks,        resolver: Queries::Tracks
+    field :playlist,      resolver: Queries::Playlist
+    field :playlists,     resolver: Queries::Playlists
+    field :my_playlists,  resolver: Queries::MyPlaylists
+    field :roles,         resolver: Queries::Roles
   end
 end
