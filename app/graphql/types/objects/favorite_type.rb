586ed814c86408803c9adbfa68602558a5e21ce4
himanushi <himanushi@users.noreply.github.com>
2021-05-19 10:19:12 +0900

    playlist お気に入り対応

--- app/graphql/types/objects/favorite_type.rb
+++ app/graphql/types/objects/favorite_type.rb
@@ -6,6 +6,7 @@ module Types
       field :artist_ids, [String], null: false, description: "アーティストID"
       field :album_ids, [String], null: false, description: "アルバムID"
       field :track_ids, [String], null: false, description: "トラックID"
+      field :playlist_ids, [String], null: false, description: "プレイリストID"
     end
   end
 end
