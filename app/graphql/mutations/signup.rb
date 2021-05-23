9a572ece217b2b3be4ecd004dd3ee1dd27367b38
himanushi <himanushi@users.noreply.github.com>
2021-05-23 10:53:01 +0900

    signup 仕様変更

--- app/graphql/mutations/signup.rb
+++ app/graphql/mutations/signup.rb
@@ -7,48 +7,38 @@ class Mutations::Signup < Mutations::BaseMutation
   argument :password_confirmation, String, required: true
 
   field :current_user, Types::Objects::CurrentUserType, null: true
-  field :error, String, null: true
 
   class RegisteredError < StandardError; end
 
   def use_recaptcha?; true end
 
   def mutate(name:, username:, password:, password_confirmation:)
-    begin
-      # 登録済みは正常終了しておく
-      raise RegisteredError if context[:current_info][:user].registered
 
-      attrs = {
-        name: name,
-        username: username,
-        password: password,
-        password_confirmation: password_confirmation,
-        registered: true,
-        role: Role.login_role
-      }
-
-      # 設定したら全てのセッションを削除し、最新セッション作成
-      ActiveRecord::Base.transaction do
-        context[:current_info][:user].update!(attrs)
-        context[:current_info][:user].sessions.delete_all
-        context[:current_info][:session] = context[:current_info][:user].sessions.create!
-      end
-
-      {
+    # 登録済みは正常終了しておく
+    if context[:current_info][:user].registered
+      return {
         current_user: context[:current_info][:user],
-        error: nil,
-      }
-    rescue RegisteredError => error
-      # 登録済みは正常終了しておく
-      {
-        current_user: context[:current_info][:user],
-        error: nil,
-      }
-    rescue => error
-      {
-        current_user: nil,
-        error: error.message,
       }
     end
+
+    attrs = {
+      name: name,
+      username: username,
+      password: password,
+      password_confirmation: password_confirmation,
+      registered: true,
+      role: Role.login_role
+    }
+
+    # 設定したら全てのセッションを削除し、最新セッション作成
+    ActiveRecord::Base.transaction do
+      context[:current_info][:user].update!(attrs)
+      context[:current_info][:user].sessions.delete_all
+      context[:current_info][:session] = context[:current_info][:user].sessions.create!
+    end
+
+    {
+      current_user: context[:current_info][:user],
+    }
   end
 end
