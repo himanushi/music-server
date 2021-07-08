5b784210dd0702d64752efd2072d694dca1b08c4
himanushi <himanushi@users.noreply.github.com>
2021-07-09 00:27:23 +0900

    prod url を環境変数へ

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,5 +97,5 @@ Rails.application.configure do
   ActiveRecord::Base.logger = Logger.new(STDOUT)
   ActiveRecord::Base.logger.level = 0
 
-  config.hosts << "video-game-music.net"
+  config.hosts << ENV['PRODUCTION_APP_URL'].gsub(/\Ahttps?:\/\//, "")
 end
--- lib/dokku.rb
+++ lib/dokku.rb
@@ -1,12 +1,12 @@
 module Dokku
   class << self
-    def configs
-      params = File.open('.env').read.gsub(/=("|)/, '="').gsub(/("|)\n/, '" ').gsub('RAILS_ENV="development"', 'RAILS_ENV="production"')
+    def configs(env = "")
+      params = File.open(".env#{env}").read.gsub(/=("|)/, '="').gsub(/("|)\n/, '" ').gsub('RAILS_ENV="development"', 'RAILS_ENV="production"')
       print "dokku config:set music-server #{params}"
     end
 
-    def keys
-      print File.open('.env').read.split("\n").map{|p|p.gsub(/=.*/, "")}.join("\n")
+    def keys(env = "")
+      print File.open(".env#{env}").read.split("\n").map{|p|p.gsub(/=.*/, "")}.join("\n")
     end
   end
 end
