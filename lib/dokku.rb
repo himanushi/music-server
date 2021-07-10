5cfd4c7e58ca9bec7a63cd360f9ad32170511132
himanushi <himanushi@users.noreply.github.com>
2021-07-10 11:20:26 +0900

    起動時に migrate をやめた

--- lib/dokku.rb
+++ lib/dokku.rb
@@ -1,12 +1,19 @@
 module Dokku
   class << self
     def configs(env = "")
-      params = File.open(".env#{env}").read.gsub(/=("|)/, '="').gsub(/("|)\n/, '" ').gsub('RAILS_ENV="development"', 'RAILS_ENV="production"')
-      print "dokku config:set music-server #{params}"
+      file_name, app_name =
+        if env.present?
+          [".env.#{env}", env]
+        else
+          [".env", "music-server"]
+        end
+
+      params = File.open(file_name).read.gsub(/=("|)/, '="').gsub(/("|)\n/, '" ').gsub('RAILS_ENV="development"', 'RAILS_ENV="production"')
+      print "dokku config:set #{app_name} #{params}"
     end
 
-    def keys(env = "")
-      print File.open(".env#{env}").read.split("\n").map{|p|p.gsub(/=.*/, "")}.join("\n")
+    def keys
+      print File.open(".env").read.split("\n").map{|p|p.gsub(/=.*/, "")}.join("\n")
     end
   end
 end
