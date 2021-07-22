3e090babe56f6333ce1c8be78fd38142c60bf05c
himanushi <himanushi@users.noreply.github.com>
2021-07-22 16:02:48 +0900

    新着アルバム取得APIは cron でやるので削除

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -24,6 +24,5 @@ module Types
     field :tally,              mutation: Mutations::Tally
     field :generate_sitemaps,  mutation: Mutations::GenerateSitemaps
     field :clear_cache,        mutation: Mutations::ClearCache
-    field :insert_new_release_albums, mutation: Mutations::InsertNewReleaseAlbums
   end
 end
