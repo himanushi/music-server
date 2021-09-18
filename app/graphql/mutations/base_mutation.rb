cd36ceb2193c88c72a35da13ecd89da3e6f6df42
himanushi <himanushi@users.noreply.github.com>
2021-09-18 19:06:54 +0900

    ios の場合は recaptcha スキップ

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -23,7 +23,7 @@ class Mutations::BaseMutation < GraphQL::Schema::RelayClassicMutation
     end
 
     # ロボット検証
-    if Rails.env.production? && use_recaptcha?
+    if Rails.env.production? && use_recaptcha? && context[:platform] == "web"
       token = context.dig(:current_info, :cookie, "reCAPTCHAv2Token")
       unless Ggl::Recaptcha.valid?(token)
         raise GraphQL::ExecutionError.new(
