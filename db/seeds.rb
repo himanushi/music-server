bf782611b69c137ed302c1f92abe1cd6ff49d566
himanushi <himanushi@users.noreply.github.com>
2021-12-31 20:41:51 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class String
   # colorization
   # ref: https://stackoverflow.com/questions/1489183/colorized-ruby-output
@@ -14,35 +16,32 @@ class String
   end
 end
 
-puts "seed 開始".red
+puts 'seed 開始'.red
 
-Role.create!(name: "admin", description: "管理者") do |role|
-  AllowedAction::ALL_ACTIONS.each do |action_name|
-    role.allowed_actions.new(name: action_name)
+::ActiveRecord::Base.transaction do
+  ::Role.create!(name: 'admin', description: '管理者') do |role|
+    ::AllowedAction.all_actions.each do |action_name|
+      role.allowed_actions.new(name: action_name)
+    end
   end
+
+  ::Role.create!(name: 'default', description: '未ログイン') do |role|
+    ::AllowedAction.default_actions.each do |action_name|
+      role.allowed_actions.new(name: action_name)
+    end
+  end
+
+  ::Role.create!(name: 'login', description: 'ログイン済み') do |role|
+    ::AllowedAction.default_actions.each do |action_name|
+      role.allowed_actions.new(name: action_name)
+    end
+  end
+
+  password = "#{::SecureRandom.hex(3)}aA1"
+  username = ::SecureRandom.hex(3)
+
+  puts "/login [username: #{username},  password: #{password}]".green
+  ::User.create!(name: 'admin', username: username, role: ::Role.find_by!(name: 'admin'), password: password)
 end
 
-Role.create!(name: "default", description: "未ログイン") do |role|
-  AllowedAction::DEFAULT_ACTIONS.each do |action_name|
-    role.allowed_actions.new(name: action_name)
-  end
-end
-
-Role.create!(name: "login", description: "ログイン済み") do |role|
-  AllowedAction::DEFAULT_ACTIONS.each do |action_name|
-    role.allowed_actions.new(name: action_name)
-  end
-end
-
-password = SecureRandom.hex(3)
-
-puts "/login [username: admin,  password: #{password}]".red
-User.create!(
-  name: "admin",
-  username: "admin",
-  description: "管理者ユーザ",
-  role: Role.find_by!(name: "admin"),
-  encrypted_password: BCrypt::Password.create(password, cost: 12)
-)
-
-puts "seed 終了".red
+puts 'seed 終了'.red
