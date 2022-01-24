ae80ee9c2b3e54573ce14ae26cf3b605d6afef99
himanushi <himanushi@users.noreply.github.com>
2022-01-24 16:33:35 +0900

    一旦 tweet params 追加

--- app/graphql/mutations/change_album_status.rb
+++ app/graphql/mutations/change_album_status.rb
@@ -5,11 +5,12 @@ module Mutations
     description 'アルバムステータス変更'
 
     argument :status, ::Types::Enums::StatusEnum, required: true, description: '変更したいステータス'
+    argument :tweet, ::GraphQL::Types::Boolean, required: true, description: 'ツイートするか'
     argument :id, ::String, required: true, description: '変更したいID'
 
     field :result, ::GraphQL::Types::Boolean, null: false
 
-    def mutate(id:, status:)
+    def mutate(id:, status:, tweet:)
       album = ::Album.find(id)
       album.status = status
       album.save!
