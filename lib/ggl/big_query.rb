3e5e4b4ad1daff47d361e06626e3fe839cdc61f9
himanushi <himanushi@users.noreply.github.com>
2022-03-28 15:57:57 +0900

    add BigQuery

---
+++ lib/ggl/big_query.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'google/cloud/bigquery'
+
+module Ggl
+  class BigQuery
+    class << self
+      def client
+        key = ::ENV['GOOGLE_BIGQUERY_JSON_KEY']
+        if key
+          key_hash = ::JSON.parse(::JSON.parse(key))
+          creds = ::Google::Cloud::Bigquery::Credentials.new(key_hash)
+        end
+        ::Google::Cloud::Bigquery.new(credentials: creds)
+      end
+
+      def get_page_views(from_date, to_date = nil)
+        to = to_date || from_date
+
+        sql = <<~SQL.gsub("\n", ' ')
+          SELECT
+          (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'page_location') AS page_location,
+          COUNT(1) AS page_views
+          FROM
+            `#{::ENV['GOOGLE_BIGQUERY_TABLE_NAME']}.events_*`
+          WHERE
+            _TABLE_SUFFIX BETWEEN '#{from_date.strftime('%Y%m%d')}' and '#{to.strftime('%Y%m%d')}'
+            AND event_name = 'page_view'
+          GROUP BY 1
+          ORDER BY 2 DESC
+        SQL
+
+        client.query(sql)
+      end
+    end
+  end
+end
