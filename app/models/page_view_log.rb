b5f6fbd4c85353c6f4b447fe9f2962c0cff6976a
himanushi <himanushi@users.noreply.github.com>
2022-04-01 19:27:58 +0900

    ログ集計

--- app/models/page_view_log.rb
+++ app/models/page_view_log.rb
@@ -6,10 +6,15 @@ class PageViewLog < ::ActiveRecord::Base
       # 早朝だとまだ昨日のデータは作成されていないため一昨日っぽくなっているがこれでOK
       # @type var yesterday: ::Time
       yesterday = ::Time.now.beginning_of_day - 2.day
+      create_from_big_query(yesterday)
+    end
+
+    def create_from_big_query(target_date)
+      return unless ::PageViewLog.where(target_date: target_date).empty?
 
       ::ActiveRecord::Base.transaction do
-        ::Ggl::BigQuery.get_page_views(yesterday).each do |result|
-          attrs = { page_location: result[:page_location], count: result[:count], target_date: yesterday }
+        ::Ggl::BigQuery.get_page_views(target_date).each do |result|
+          attrs = { page_location: result[:page_location], count: result[:count], target_date: target_date }
           create!(**attrs)
         end
       end
