c74d1fd8dffbdd6a2a990e625619b9ba261ac1e8
himanushi <himanushi@users.noreply.github.com>
2022-04-03 10:37:49 +0900

    pv 更新

--- app/models/page_view_log.rb
+++ app/models/page_view_log.rb
@@ -19,5 +19,38 @@ class PageViewLog < ::ActiveRecord::Base
         end
       end
     end
+
+    def update_page_view_colmuns
+      results = { artists: ::Hash.new(0), albums: ::Hash.new(0), tracks: ::Hash.new(0), playlist: ::Hash.new(0) }
+
+      ::PageViewLog.all.each do |pv|
+        results.each_key do |key|
+          next unless (match = pv.page_location.match(%r{/#{key}/(?<id>.*)}))
+
+          id = match[:id]
+          results[key][id] += pv.count
+        end
+      end
+
+      ::ActiveRecord::Base.transaction do
+        results.each_key do |table_name|
+          next unless results[table_name].present?
+
+          t2 = results[table_name].map { |id, pv| "select '#{id}' as id, '#{pv}' as pv" }
+                                  .join(' union all ')
+          ::ActiveRecord::Base.connection.execute(<<~SQL)
+            UPDATE
+            #{table_name.to_s.pluralize} t1,
+            (
+              #{t2}
+            ) t2
+            SET
+            t1.pv = t2.pv
+            WHERE
+            t1.id = t2.id
+          SQL
+        end
+      end
+    end
   end
 end
