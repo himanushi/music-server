a0ad3929fa87fcf1f6d5be5d2b5b3a2cf03179cb
himanushi <himanushi@users.noreply.github.com>
2022-04-04 16:41:09 +0900

    build

---
+++ public/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "pages": "src/pages",
+  "sourceDir": "public",
+  "routifyDir": ".routify",
+  "ignore": "",
+  "dynamicImports": true,
+  "singleBuild": true,
+  "noHashScroll": false,
+  "distDir": "dist",
+  "hashScroll": true,
+  "extensions": [
+    "html",
+    "svelte",
+    "md",
+    "svx"
+  ],
+  "started": "2022-04-04T07:40:41.618Z"
+}
