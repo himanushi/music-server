12db2048296d98ae4f0cc01c59d2d837dc704034
himanushi <himanushi@users.noreply.github.com>
2022-04-10 21:09:09 +0900

    アルバムトラックの不正を修正

--- app/graphql/queries/album_query.rb
+++ app/graphql/queries/album_query.rb
@@ -9,10 +9,15 @@ module Queries
     argument :id, ::String, required: true, description: 'ID'
 
     def query(id:)
-      ::Album.includes(tracks: :apple_music_tracks).order(
-        'apple_music_tracks.disc_number': :asc,
-        'apple_music_tracks.track_number': :asc
-      ).find_by(id: id)
+      album = ::Album.find_by(id: id)
+      return unless (apple_music_album = album&.apple_music_album)
+
+      ::Album.includes(tracks: :apple_music_tracks)
+             .where('apple_music_tracks.apple_music_album_id = :id', { id: apple_music_album.id })
+             .order(
+               'apple_music_tracks.disc_number': :asc,
+               'apple_music_tracks.track_number': :asc
+             ).find_by(id: id)
     end
   end
 end
