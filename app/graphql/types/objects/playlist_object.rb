e724f255555521a01127085760da3eabbe552ef6
himanushi <himanushi@users.noreply.github.com>
2022-04-22 08:47:18 +0900

    add is_condition prop

--- app/graphql/types/objects/playlist_object.rb
+++ app/graphql/types/objects/playlist_object.rb
@@ -5,16 +5,17 @@ module Types
     class PlaylistObject < ::Types::Objects::BaseObject
       description 'プレイリスト'
 
-      field :id,          ::String, null: false, description: 'ID'
-      field :name,        ::String, null: false, description: 'タイトル'
-      field :description, ::String, null: false, description: '説明'
-      field :public_type, ::Types::Enums::PlaylistPublicTypeEnum, null: false, description: '公開種別'
-      field :is_mine,     ::GraphQL::Types::Boolean, null: true, description: '自身のプレイリストか判定'
-      field :created_at,  ::GraphQL::Types::ISO8601DateTime, null: false, description: '作成日'
-      field :updated_at,  ::GraphQL::Types::ISO8601DateTime, null: false, description: '更新日'
-      field :author,      ::Types::Objects::UserObject, null: true, description: '作者'
-      field :track,       ::Types::Objects::TrackObject, null: true, description: 'ジャケットトラック'
-      field :items,       [::Types::Objects::PlaylistItemObject], null: false, description: '曲一覧'
+      field :id,           ::String, null: false, description: 'ID'
+      field :name,         ::String, null: false, description: 'タイトル'
+      field :description,  ::String, null: false, description: '説明'
+      field :is_condition, ::GraphQL::Types::Boolean, null: false, description: '条件プレイリスト判定'
+      field :public_type,  ::Types::Enums::PlaylistPublicTypeEnum, null: false, description: '公開種別'
+      field :is_mine,      ::GraphQL::Types::Boolean, null: true, description: '自身のプレイリストか判定'
+      field :created_at,   ::GraphQL::Types::ISO8601DateTime, null: false, description: '作成日'
+      field :updated_at,   ::GraphQL::Types::ISO8601DateTime, null: false, description: '更新日'
+      field :author,       ::Types::Objects::UserObject, null: true, description: '作者'
+      field :track,        ::Types::Objects::TrackObject, null: true, description: 'ジャケットトラック'
+      field :items,        [::Types::Objects::PlaylistItemObject], null: false, description: '曲一覧'
 
       def is_mine = object.user == context[:current_info][:user]
 
