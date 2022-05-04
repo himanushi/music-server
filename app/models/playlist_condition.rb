c9661d5dc813af31959a92f30032b588a2322887
himanushi <himanushi@users.noreply.github.com>
2022-05-04 10:40:33 +0900

    名前順だと速度が遅いので作成順にした

--- app/models/playlist_condition.rb
+++ app/models/playlist_condition.rb
@@ -3,6 +3,6 @@
 class PlaylistCondition < ::ActiveRecord::Base
   belongs_to :playlist
 
-  enum order: { popularity: 0, release_date: 1, name: 2 }, _prefix: true
+  enum order: { popularity: 0, release_date: 1, created_at: 2 }, _prefix: true
   enum direction: { asc: 0, desc: 1 }, _prefix: true
 end
